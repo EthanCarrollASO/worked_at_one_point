7db1a6660b07824a2ed3b57b63488fb04b0432c2
EthanCarrollASO <141178467+EthanCarrollASO@users.noreply.github.com>
2023-09-02 17:30:07 -0600

    Add files via upload

--- from_c.c
+++ from_c.c
@@ -12,17 +12,14 @@ int main(){
     int i =0;
     int size = a_p->size;
     for(i=0;i<size;i++){
-        //printf("%d\n", a_p->struct_xpix[i]);
-        //printf("%d\n", a_p->struct_ypix[i]);
-        //printf("%f\n", a_p->struct_s[i]);
+        printf("%d\n", a_p->struct_xpix[i]);
+        printf("%d\n", a_p->struct_ypix[i]);
+        //printf("%f\n", a_p->struct_s[i]);   // This line throws seg fault errors
     }
 
     printf("\nFROM MAIN\n========================================\n");
-    printf("%p\n", &(a_p->struct_s[0]) );
+    printf("%p: %f\n", &(a_p->struct_s[0]), a_p->struct_s[0]);
     printf("%p\n", &(a_p->struct_s[0]));                                //Why does printing the same value twice give two differient results?
-
-    //printf("%p\n", (a_p->struct_s[1]));
-    //printf("%p\n", &(a_p->struct_s[2]));
     
     return 0;
 }
